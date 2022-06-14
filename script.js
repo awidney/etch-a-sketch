b9e76279f57305c3539289832111eedec51e2ca1
awidney <adamwidney@hotmail.ca>
2022-06-14 15:00:42 -0700

    Add event listeners, redefine grid upon prompt
    Added mouseover event listener to pixels which adds color-fill class.
    Added click event listener to button which prompts for a grid size.
    Old grid is removed, new grid is created with a redefined pixel size.
    mouseover event listeners are added to new grid. Grid size redefined.

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
 const container = document.querySelector('.container');
 
-// inject 16x16(256) child <div> elements into container
+// create 256 (16*16) child elements
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
-    div.setAttribute('id', 'cell');     
-    div.textContent = 'cell';
+    div.setAttribute('id', 'pixel');     
     container.appendChild(div);
 }
+
+const pixels = document.querySelectorAll('#pixel');
+
+// add event listeners to pixels
+pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', () => {
+        pixel.classList.add('color-fill');
+    });
+});
+
+const button = document.querySelector('button');
+
+// add event listener to button
+button.addEventListener('click', () => {
+    // prompt for new grid size
+    const answer = prompt('How many squares per side? Limit: 100');
+    
+    if (answer > 100) {
+        alert('Limit: 100');
+    } else if (!answer) {
+        return;
+    } else {
+        const children = container.childElementCount;
+        
+        // remove all pixels within container
+        for (let i = 0; i < children; i++) {
+            const div = document.querySelector('div>div'); 
+            container.removeChild(div);
+        }
+        
+        // add pixels to container with redefined size, add event listeners
+        for (let i = 0; i < answer * answer; i++) {
+            const div = document.createElement('div');
+            div.setAttribute('id', 'pixelNew')
+            div.setAttribute('style', `width: ${544/answer}; height: ${544/answer};`);
+            container.appendChild(div);
+            
+            const pixel = document.querySelector('#pixelNew:last-of-type');
+            pixel.addEventListener('mouseover', () => {
+                pixel.classList.add('color-fill');
+            });
+        }
+        
+        // redefine number of grid columns, rows
+        const containerStyle = document.querySelector('.container').style;
+        containerStyle.gridTemplateColumns = `repeat(${answer}, ${544/answer}px)`;
+        containerStyle.gridTemplateRows = `repeat(${answer}, ${544/answer}px)`;
+    }
+});
